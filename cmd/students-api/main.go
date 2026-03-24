0288f1ee96be4415552c462896cd8216fe3edf14
Brijesh Singh <coderrrx@gmail.com>
2026-03-24 20:51:12 +0530

    intialised server

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,6 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/brijesh025/students-api/internal/config"
+)
 func main(){
-	fmt.Println("Hello, World!")
+	//load config
+	cfg := config.MustLoad();
+	// database setup
+	// setup router
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to sstudents api"))
+	})
+	// setup server
+	server := http.Server{
+		Addr: cfg.HTTPServer.Address,
+		Handler: router,
+	}
+	err := server.ListenAndServe()
+	if (err != nil) {
+		log.Fatal("Failed to start server")
+	}
+	fmt.Println("Server started and Ready to work!!")
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,12 +1,46 @@
 package config
 
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
 type HTTPServer struct {
 	Address string `yaml:"address"`
 	Timeout int    `yaml:"timeout"`
 }
- //env-default: "production"
+
+// env-default: "production"
 type Config struct {
-	Env string `yaml: "env" env: "ENV" env-required: "true"`  //struct tags
-	StoragePath string `yaml: "storage_path" env-required:"true"`
-	HTTPServer HTTPServer `yaml:"http_server"`
+	Env         string     `yaml: "env" env: "ENV" env-required: "true" env-default` //struct tags
+	StoragePath string     `yaml: "storage_path" env-required:"true"`
+	HTTPServer  HTTPServer `yaml:"http_server"` // these tags link to the same variables in other configuration files
+}
+
+func MustLoad() *Config{
+	var configPath string=  os.Getenv(("CONFIG_PATH"))
+	if (configPath == ""){
+		flags := flag.String("config", "", "path to the configuration file")
+		flag.Parse()
+		configPath = *flags
+		if (configPath == ""){
+			log.Fatal("Config path is not set");
+		}
+	}
+
+	_, e := os.Stat(configPath) // return the information of the passed file
+	if(os.IsNotExist(e)){
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+
+	var cfg Config;
+	err := cleanenv.ReadConfig(configPath, &cfg);
+	if(err!=nil){
+		log.Fatalf("can not read config file: %s", err.Error())
+	}
+	return &cfg
+
 }
