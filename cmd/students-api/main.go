48684bf696ff704d0e49a18b82698f2694e600d4
Brijesh Singh <coderrrx@gmail.com>
2026-03-27 20:32:15 +0530

    made server to shutdown slowly

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -4,26 +4,47 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	// "sync"
 
 	"github.com/brijesh025/students-api/internal/config"
+	"github.com/joho/godotenv"
 )
 func main(){
 	//load config
+	e := godotenv.Load();
+	if(e!=nil){
+		log.Println(".env file was not loaded")
+	}
 	cfg := config.MustLoad();
 	// database setup
 	// setup router
 	router := http.NewServeMux()
 	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to sstudents api"))
+		w.Write([]byte("Welcome to students api"))
 	})
 	// setup server
 	server := http.Server{
 		Addr: cfg.HTTPServer.Address,
 		Handler: router,
 	}
-	err := server.ListenAndServe()
-	if (err != nil) {
-		log.Fatal("Failed to start server")
-	}
-	fmt.Println("Server started and Ready to work!!")
+	fmt.Printf("Server started and Ready to work!! Check %s", cfg.HTTPServer.Address)
+	// var wg sync.WaitGroup
+	// wg.Add(1);
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM);
+	var err error
+	go func(/*wg *sync.WaitGroup*/){
+		// defer wg.Done()
+		err = server.ListenAndServe()
+		if (err != nil) {
+			log.Fatal("Failed to start server")
+		}
+	}(/*&wg*/)
+	// wg.Wait()
+	<-done
+	fmt.Println("this is after locking the server listening funciton")
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -4,20 +4,20 @@ import (
 	"flag"
 	"log"
 	"os"
+	"time"
 
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type HTTPServer struct {
-	Address string `yaml:"address"`
-	Timeout int    `yaml:"timeout"`
+	Address string        `yaml:"address" env-required:"true"`
+	Timeout time.Duration `yaml:"timeout"`
 }
 
-// env-default: "production"
 type Config struct {
-	Env         string     `yaml: "env" env: "ENV" env-required: "true" env-default` //struct tags
-	StoragePath string     `yaml: "storage_path" env-required:"true"`
-	HTTPServer  HTTPServer `yaml:"http_server"` // these tags link to the same variables in other configuration files
+	Env         string     `yaml:"env" env:"ENV" env-default:"production"` // struct tags
+	StoragePath string     `yaml:"storage_path" env-required:"true"`        // these tags link to the same variables in other configuration files
+	HTTPServer  HTTPServer `yaml:"http_server"`
 }
 
 func MustLoad() *Config{
