ead9950a0c43ba1be52730a5f20bc1247d797ca6
Brijesh Singh <coderrrx@gmail.com>
2026-05-22 14:04:08 +0530

    intialised routes and handlers, completed crud

--- internal/http/handlers/student/students.go
+++ internal/http/handlers/student/students.go
@@ -1,4 +1,4 @@
-package students
+package student
 
 import (
 	"encoding/json"
@@ -6,29 +6,71 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
+	"strconv"
 
 	"github.com/brijesh025/students-api/internal/types"
 	"github.com/brijesh025/students-api/internal/utils/response"
+	"github.com/brijesh025/students-api/internal/storage"
+	"github.com/go-playground/validator/v10"
 )
 
-func Create() http.HandlerFunc {
+func New(storage storage.Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		slog.Info("Creating a Student")
+		slog.Info("creating a student")
+
 		var student types.Student
 		err := json.NewDecoder(r.Body).Decode(&student)
-		// if err!=nil {
-		// 	slog.Error("decoding failed")
-		// }
-		if errors.Is(err, io.EOF){
+		if errors.Is(err, io.EOF) {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(errors.New("request body is empty")))
+			return
+		}
+		if err != nil {
 			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
 			return
 		}
-		if(err != nil){
-			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+
+		if err := validator.New().Struct(student); err != nil {
+			validateErrs := err.(validator.ValidationErrors)
+			response.WriteJson(w, http.StatusBadRequest, response.ValidationError(validateErrs))
 			return
 		}
-		// ------------------> we should always validate our request made by user at our end
-		
-		response.WriteJson(w, http.StatusCreated, map[string] interface{} {"sucsess": "OK", "student": student})
+
+		id, err := storage.CreateStudent(student.Name, student.Email, student.Age)
+		if err != nil {
+			response.WriteJson(w, http.StatusInternalServerError, response.GeneralError(err))
+			return
+		}
+
+		response.WriteJson(w, http.StatusCreated, map[string]int64{"id": id})
+	}
+}
+
+func GetById(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id, err := strconv.ParseInt(r.PathValue("id"), 10, 64)
+		if err != nil {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(errors.New("invalid student id")))
+			return
+		}
+
+		student, err := storage.GetStudentById(id)
+		if err != nil {
+			response.WriteJson(w, http.StatusNotFound, response.GeneralError(err))
+			return
+		}
+
+		response.WriteJson(w, http.StatusOK, student)
+	}
+}
+
+func GetList(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		students, err := storage.GetStudents()
+		if err != nil {
+			response.WriteJson(w, http.StatusInternalServerError, response.GeneralError(err))
+			return
+		}
+
+		response.WriteJson(w, http.StatusOK, students)
 	}
 }
